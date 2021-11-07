839ef83e3ba3c6e2dc8e539eff11d8b432e3afad
matheus-fls <matheus_fls@yahoo.com.br>
2021-11-06 22:25:53 -0300

    Add api members route

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Members");
 
 const app = express();
+
+// Gets all members
+app.get("/api/members", (_req, res) => res.json(members));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Set routes manually
-// app.get("/", (req, res) => {
+// app.get("/", (_req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
 
 // // This route fixes the 404 message on the browser console
-// app.get("/images/favicon.ico", (req, res) => {
+// app.get("/images/favicon.ico", (_req, res) => {
 //   res.sendFile(path.join(__dirname, "images", "favicon.ico"));
 // });
 
