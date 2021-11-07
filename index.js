f1023fbd39f9708ce906860cbc2f017863fc8c8b
matheus-fls <matheus_fls@yahoo.com.br>
2021-11-06 22:17:17 -0300

    Set static folder

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Set routes manually
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// // This route fixes the 404 message on the browser console
+// app.get("/images/favicon.ico", (req, res) => {
+//   res.sendFile(path.join(__dirname, "images", "favicon.ico"));
+// });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Stopped @ 16:11
