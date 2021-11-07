334e8b4b0b0545c485e237ca1f93dc0481798575
matheus-fls <matheus_fls@yahoo.com.br>
2021-11-06 22:36:58 -0300

    Add middleware function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 const members = require("./Members");
-
+const logger = require("./middleware/logger");
 const app = express();
+
+// Init middleware
+// app.use(logger);
 
 // Gets all members
 app.get("/api/members", (_req, res) => res.json(members));
